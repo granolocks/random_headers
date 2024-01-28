a961842f3edd4c6ec13cedfd1d9360ff8287b21c
Gabe Koss <gabe.koss@gmail.com>
2024-01-28 12:06:44 -0500

    autoformat

--- logger.h
+++ logger.h
@@ -9,27 +9,32 @@ enum LL_LogLevel {
   LL_Warn,
   LL_Info,
   LL_Debug
-}; 
+};
 
 char* LL_to_s(int ll);
 
 // https://gcc.gnu.org/onlinedocs/cpp/Variadic-Macros.html
 // the ##__VA_ARGS__ macros bit is important!
-#define LL(level, format_string, ...) {\
-  printf("[%-8s]: " format_string "\n", LL_to_s(level), ##__VA_ARGS__);\
-}
+#define LL(level, format_string, ...) \
+  { printf("[%-8s]: " format_string "\n", LL_to_s(level), ##__VA_ARGS__); }
 
 #ifdef LL_IMPL
 char* LL_to_s(int ll) {
-  switch(ll)
-  {
-    case LL_Fatal:    return "FATAL";
-    case LL_Critical: return "CRITICAL";
-    case LL_Error:    return "ERROR";
-    case LL_Warn:     return "WARN";
-    case LL_Info:     return "INFO";
-    case LL_Debug:    return "DEBUG";
-    default:          return "????";
+  switch (ll) {
+    case LL_Fatal:
+      return "FATAL";
+    case LL_Critical:
+      return "CRITICAL";
+    case LL_Error:
+      return "ERROR";
+    case LL_Warn:
+      return "WARN";
+    case LL_Info:
+      return "INFO";
+    case LL_Debug:
+      return "DEBUG";
+    default:
+      return "????";
   }
 }
 #endif
--- matrix.h
+++ matrix.h
@@ -1,49 +1,51 @@
 #ifndef MATRIX_H
+
 #define MATRIX_H
 
 //
 // USE AT YOUR OWN RISK ^_^'
 //
 
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
 // Allocate a matrix of WxH elements and of element with size == to type T
 #define MATRIX(w, h, T) matrix_alloc(w, h, sizeof(T))
 
 // Retrieve element of type T at coordinate x,y from matrix
-#define MATRIX_GET(matrix, T, x, y)\
-  (T) ((T*) matrix->elements)[matrix->w * y + x]
+#define MATRIX_GET(matrix, T, x, y) (T)((T*)matrix->elements)[matrix->w * y + x]
 
 // Set element of type T with some value at coordinate x,y into matrix
-#define MATRIX_SET(matrix, T, x, y, val)\
-  ((T*) matrix->elements)[matrix->w * y + x] = val 
+#define MATRIX_SET(matrix, T, x, y, val) \
+  ((T*)matrix->elements)[matrix->w * y + x] = val
 
-// iterate across each eleement of matrix and call some function passed to the macro
-// fn signature should look like void some_fn(Matrix* m, size_t index, T* element);
+// iterate across each eleement of matrix and call some function passed to the
+// macro fn signature should look like void some_fn(Matrix* m, size_t index, T*
+// element);
 //
-// note this is called _MAP but really it is just an iterator helper and doesn't 
-// create a new matrix or modify the original unless specified in the provided fn
-#define MATRIX_MAP(matrix, T, fn) \
-  for(size_t i = 0; i < matrix->w * matrix->h; i++) {\
-    fn(matrix, i, &((T*) matrix->elements)[i]);\
+// note this is called _MAP but really it is just an iterator helper and doesn't
+// create a new matrix or modify the original unless specified in the provided
+// fn
+#define MATRIX_MAP(matrix, T, fn)                      \
+  for (size_t i = 0; i < matrix->w * matrix->h; i++) { \
+    fn(matrix, i, &((T*)matrix->elements)[i]);         \
   }
 
-// Generates a function to add together two matrices of type T. 
+// Generates a function to add together two matrices of type T.
 // Must have the same dimensions and element size.
 //
 // In practice the second matrix (b) will be added into (a) destructively and
 // so (a) is mutated by this operation
-#define MATRIX_ADDER(adder, T) \
-void adder(Matrix* a, Matrix* b) {\
-  assert(a->w == b->w);\
-  assert(a->h == b->h);\
-  assert(a->sz == b->sz);\
-  for(size_t i = 0; i < matrix_element_len(a); i++) {\
-    ((T*) a->elements)[i] += ((T*) b->elements)[i];\
-  }\
-}
+#define MATRIX_ADDER(adder, T)                           \
+  void adder(Matrix* a, Matrix* b) {                     \
+    assert(a->w == b->w);                                \
+    assert(a->h == b->h);                                \
+    assert(a->sz == b->sz);                              \
+    for (size_t i = 0; i < matrix_element_len(a); i++) { \
+      ((T*)a->elements)[i] += ((T*)b->elements)[i];      \
+    }                                                    \
+  }
 
 struct Matrix {
   // # of columns
@@ -61,9 +63,7 @@ struct Matrix {
 typedef struct Matrix Matrix;
 
 // get # elements
-size_t matrix_element_len(Matrix* matrix) {
-  return matrix->w * matrix->h;
-}
+size_t matrix_element_len(Matrix* matrix) { return matrix->w * matrix->h; }
 
 // get # bytes for elements
 size_t matrix_element_len_bytes(Matrix* matrix) {
@@ -73,7 +73,7 @@ size_t matrix_element_len_bytes(Matrix* matrix) {
 // alloc space for a matrix and its elements, return pointer
 Matrix* matrix_alloc(size_t w, size_t h, size_t sz) {
   Matrix* matrix;
-  matrix = (Matrix*) malloc(sizeof(Matrix));
+  matrix = (Matrix*)malloc(sizeof(Matrix));
   matrix->w = w;
   matrix->h = h;
   matrix->sz = sz;
@@ -93,7 +93,7 @@ Matrix* matrix_clone(Matrix* orig) {
   return new_matrix;
 }
 
-// release me 
+// release me
 void matrix_free(Matrix* matrix) {
   free(matrix->elements);
   free(matrix);
